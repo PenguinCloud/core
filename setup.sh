e1b1ce8d73edc519acded7188bb5df8ab9775198
shades <justin@penguinzmedia.group>
2021-07-31 08:44:39 -0700

    Ansible still fails on first run due to host key check.

--- setup.sh
+++ setup.sh
@@ -25,9 +25,9 @@ fi
 export KUBECONFIG=/root/.kube/config
 
 if [ -z $1 ]; then 
-ansible-playbook  upstart.yaml
+ansible-playbook -e 'host_key_checking=False' upstart.yaml
 else
-ansible-playbook  upstart.yaml --tags $1
+ansible-playbook -e 'host_key_checking=False' upstart.yaml --tags $1
 fi
 # "resolvectl status" can be used to validate DNS applied (you usually have to scroll down to see them)
 # We have it set to cloudflare as they are our primary dns host for PMG prod
